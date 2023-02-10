968995c2597725781c1029ba5cab58b3e678034c
Petri-Johan Last <petri.last@sourcegraph.com>
2023-02-10 07:46:21 +0200

    Render and navigate tabs correctly

--- main.c
+++ main.c
@@ -2,14 +2,22 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+#define TAB_STOP 4
+
 struct row {
+  /* Actual file contents. */
   char *chars;
   int length;
+
+  /* Contents to render. */
+  char *rchars;
+  int rlength;
 };
 
 struct file {
@@ -21,10 +29,12 @@ struct editor {
   char *filename;
   struct file file;
   struct render_buffer render_buffer;
+  struct row *current_row;
   int screen_rows;
   int screen_cols;
   int file_cursor_row;
   int file_cursor_col;
+  int render_cursor_col;
   int render_row_offset;
   int render_col_offset;
 };
@@ -43,14 +53,38 @@ void editor_open_file(struct editor *E, char *filename) {
 
       E->file.rows =
           realloc(E->file.rows, sizeof(struct row) * (E->file.length + 1));
+
       E->file.rows[E->file.length].chars = malloc(linelen + 1);
       E->file.rows[E->file.length].length = linelen;
       memcpy(E->file.rows[E->file.length].chars, line, linelen);
+      int tabs = 0;
+      for (int i = 0; i <= linelen; i++) {
+        E->file.rows[E->file.length].chars[i] = line[i];
+        if (line[i] == '\t')
+          tabs++;
+      }
       E->file.rows[E->file.length].chars[linelen] = '\0';
+
+      E->file.rows[E->file.length].rchars =
+          malloc(linelen + tabs * (TAB_STOP - 1) + 1);
+      E->file.rows[E->file.length].rlength = linelen + tabs * (TAB_STOP - 1);
+
+      int i = 0;
+      for (int j = 0; j < linelen; j++) {
+        if (line[j] == '\t') {
+          for (int k = 0; k < TAB_STOP; k++)
+            E->file.rows[E->file.length].rchars[i++] = ' ';
+        } else {
+          E->file.rows[E->file.length].rchars[i++] = line[j];
+        }
+      }
+      E->file.rows[E->file.length].rchars[i] = '\0';
+
       E->file.length++;
     }
   }
 
+  E->current_row = &E->file.rows[0];
   E->filename = filename;
   fclose(fp);
 }
@@ -62,6 +96,7 @@ void editor_close(struct editor *E) {
 
   for (int i = 0; i < E->file.length; i++) {
     free(E->file.rows[i].chars);
+    free(E->file.rows[i].rchars);
   }
   free(E->file.rows);
   render_buffer_free(&E->render_buffer);
@@ -77,8 +112,130 @@ char editor_read_key() {
   return c;
 }
 
+void set_render_column(const char *chars, int length, int *file_col,
+                       int *render_col, int preferred_col) {
+  *render_col = 0;
+  *file_col = 0;
+  if (chars[0] == '\t')
+    *render_col += TAB_STOP - 1;
+
+  while (*render_col < preferred_col && *file_col < length - 1) {
+    if (chars[++(*file_col)] == '\t')
+      *render_col += TAB_STOP - 1;
+
+    (*render_col)++;
+  }
+}
+
+int editor_process_input(struct editor *E) {
+  char c = editor_read_key();
+
+  switch (c) {
+  case '\033':
+    if (read(STDIN_FILENO, &c, 1) != 1)
+      break;
+    if (read(STDIN_FILENO, &c, 1) != 1)
+      break;
+    switch (c) {
+    case 'A':
+      c = 'k';
+      break;
+    case 'B':
+      c = 'j';
+      break;
+    }
+    break;
+  }
+
+  switch (c) {
+  case 'q':
+    return 0;
+  case 'k':
+    if (E->file_cursor_row > 0) {
+      E->current_row--;
+      E->file_cursor_row--;
+      render_buffer_append(&E->render_buffer, "\033M", 3);
+      if (E->file_cursor_row < E->render_row_offset) {
+        E->render_row_offset--;
+        render_buffer_append(&E->render_buffer, "\033[s\r", 4);
+        render_buffer_append(&E->render_buffer, E->current_row->rchars,
+                             E->current_row->rlength);
+        render_buffer_append(&E->render_buffer, "\033[K", 3);
+        render_buffer_append(&E->render_buffer, "\033[u", 3);
+      }
+
+      int preferred_col = E->render_cursor_col;
+      set_render_column(E->current_row->chars, E->current_row->length,
+                        &E->file_cursor_col, &E->render_cursor_col,
+                        preferred_col);
+
+      render_set_cursor_position(&E->render_buffer,
+                                 E->file_cursor_row - E->render_row_offset + 1,
+                                 E->render_cursor_col + 1);
+    }
+    break;
+  case 'j': {
+    if (E->file_cursor_row < E->file.length - 1) {
+      E->file_cursor_row++;
+      E->current_row++;
+      render_buffer_append(&E->render_buffer, "\033D", 3);
+      if (E->file_cursor_row > E->render_row_offset + E->screen_rows - 1) {
+        E->render_row_offset++;
+        render_buffer_append(&E->render_buffer, "\033[s\r", 4);
+        render_buffer_append(&E->render_buffer, E->current_row->rchars,
+                             E->current_row->rlength);
+        render_buffer_append(&E->render_buffer, "\033[K", 3);
+        render_buffer_append(&E->render_buffer, "\033[u", 3);
+      }
+
+      int preferred_col = E->render_cursor_col;
+      set_render_column(E->current_row->chars, E->current_row->length,
+                        &E->file_cursor_col, &E->render_cursor_col,
+                        preferred_col);
+
+      render_set_cursor_position(&E->render_buffer,
+                                 E->file_cursor_row - E->render_row_offset + 1,
+                                 E->render_cursor_col + 1);
+    }
+    break;
+  }
+  case 'l':
+    if (E->file_cursor_col < E->current_row->length - 1) {
+      E->file_cursor_col++;
+      if (E->current_row->chars[E->file_cursor_col] == '\t') {
+        E->render_cursor_col += TAB_STOP;
+        char term_command[16];
+        int len =
+            snprintf(term_command, sizeof(term_command), "\033[%dC", TAB_STOP);
+        render_buffer_append(&E->render_buffer, term_command, len);
+      } else {
+        E->render_cursor_col++;
+        render_buffer_append(&E->render_buffer, "\033[C", 3);
+      }
+    }
+    break;
+  case 'h':
+    if (E->file_cursor_col > 0) {
+      if (E->current_row->chars[E->file_cursor_col] == '\t') {
+        E->render_cursor_col -= TAB_STOP;
+        char term_command[16];
+        int len =
+            snprintf(term_command, sizeof(term_command), "\033[%dD", TAB_STOP);
+        render_buffer_append(&E->render_buffer, term_command, len);
+      } else {
+        E->render_cursor_col--;
+        render_buffer_append(&E->render_buffer, "\033[D", 3);
+      }
+      E->file_cursor_col--;
+    }
+    break;
+  }
+
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
-  struct editor E = {NULL, {NULL, 0}, {NULL, 0}, 0, 0, 0, 0, 0, 0};
+  struct editor E = {NULL, {NULL, 0}, {NULL, 0}, NULL, 0, 0, 0, 0, 0, 0, 0};
 
   if (argc < 2) {
     return 1;
@@ -98,136 +255,27 @@ int main(int argc, char *argv[]) {
 
   render_set_cursor_home(&E.render_buffer);
 
-  for (int i = 0; i < E.screen_rows; i++) {
-    render_buffer_append(&E.render_buffer, E.file.rows[i].chars,
-                         E.file.rows[i].length);
+  render_buffer_append(&E.render_buffer, "\x1b[2J", 4);
+
+  for (int i = 0; i < E.screen_rows && i < E.file.length; i++) {
+    render_buffer_append(&E.render_buffer, E.file.rows[i].rchars,
+                         E.file.rows[i].rlength);
     render_buffer_append(&E.render_buffer, "\033[K", 3);
     if (i < E.screen_rows - 1) {
       render_buffer_append(&E.render_buffer, "\r\n", 2);
     }
   }
 
-  render_set_cursor_home(&E.render_buffer);
+  set_render_column(E.current_row->chars, E.current_row->length,
+                    &E.file_cursor_col, &E.render_cursor_col, 0);
+  render_set_cursor_position(&E.render_buffer,
+                             E.file_cursor_row - E.render_row_offset + 1,
+                             E.render_cursor_col + 1);
 
   render_buffer_write(&E.render_buffer);
 
   /* Main loop. */
-  char c;
-  while (1) {
-    c = editor_read_key();
-    switch (c) {
-    case '\033':
-      if (read(STDIN_FILENO, &c, 1) != 1)
-        break;
-      if (read(STDIN_FILENO, &c, 1) != 1)
-        break;
-      switch (c) {
-      case 'A':
-        if (E.file_cursor_row > 0) {
-          E.file_cursor_row--;
-          render_buffer_append(&E.render_buffer, "\033M", 3);
-          if (E.file_cursor_row < E.render_row_offset) {
-            E.render_row_offset--;
-            render_buffer_append(&E.render_buffer, "\033[s\r", 4);
-            render_buffer_append(&E.render_buffer,
-                                 E.file.rows[E.file_cursor_row].chars,
-                                 E.file.rows[E.file_cursor_row].length);
-            render_buffer_append(&E.render_buffer, "\033[K", 3);
-            render_buffer_append(&E.render_buffer, "\033[u", 3);
-          }
-          if (E.file_cursor_col >= E.file.rows[E.file_cursor_row].length) {
-            E.file_cursor_col =
-                MAX(E.file.rows[E.file_cursor_row].length - 1, 0);
-            render_set_cursor_position(
-                &E.render_buffer, E.file_cursor_row - E.render_row_offset + 1,
-                E.file_cursor_col + 1);
-          }
-        }
-        break;
-      case 'B':
-        if (E.file_cursor_row < E.file.length - 1) {
-          E.file_cursor_row++;
-          render_buffer_append(&E.render_buffer, "\033D", 3);
-          if (E.file_cursor_row > E.render_row_offset + E.screen_rows - 1) {
-            E.render_row_offset++;
-            render_buffer_append(&E.render_buffer, "\033[s\r", 4);
-            render_buffer_append(&E.render_buffer,
-                                 E.file.rows[E.file_cursor_row].chars,
-                                 E.file.rows[E.file_cursor_row].length);
-            render_buffer_append(&E.render_buffer, "\033[K", 3);
-            render_buffer_append(&E.render_buffer, "\033[u", 3);
-          }
-          if (E.file_cursor_col >= E.file.rows[E.file_cursor_row].length) {
-            E.file_cursor_col =
-                MAX(E.file.rows[E.file_cursor_row].length - 1, 0);
-            render_set_cursor_position(
-                &E.render_buffer, E.file_cursor_row - E.render_row_offset + 1,
-                E.file_cursor_col + 1);
-          }
-        }
-        break;
-      }
-      break;
-    case 'q':
-      editor_close(&E);
-      render_termios_set(&orig_termios);
-      return 0;
-    case 'k':
-      if (E.file_cursor_row > 0) {
-        E.file_cursor_row--;
-        render_buffer_append(&E.render_buffer, "\033M", 3);
-        if (E.file_cursor_row < E.render_row_offset) {
-          E.render_row_offset--;
-          render_buffer_append(&E.render_buffer, "\033[s\r", 4);
-          render_buffer_append(&E.render_buffer,
-                               E.file.rows[E.file_cursor_row].chars,
-                               E.file.rows[E.file_cursor_row].length);
-          render_buffer_append(&E.render_buffer, "\033[K", 3);
-          render_buffer_append(&E.render_buffer, "\033[u", 3);
-        }
-        if (E.file_cursor_col >= E.file.rows[E.file_cursor_row].length) {
-          E.file_cursor_col = MAX(E.file.rows[E.file_cursor_row].length - 1, 0);
-          render_set_cursor_position(
-              &E.render_buffer, E.file_cursor_row - E.render_row_offset + 1,
-              E.file_cursor_col + 1);
-        }
-      }
-      break;
-    case 'j':
-      if (E.file_cursor_row < E.file.length - 1) {
-        E.file_cursor_row++;
-        render_buffer_append(&E.render_buffer, "\033D", 3);
-        if (E.file_cursor_row > E.render_row_offset + E.screen_rows - 1) {
-          E.render_row_offset++;
-          render_buffer_append(&E.render_buffer, "\033[s\r", 4);
-          render_buffer_append(&E.render_buffer,
-                               E.file.rows[E.file_cursor_row].chars,
-                               E.file.rows[E.file_cursor_row].length);
-          render_buffer_append(&E.render_buffer, "\033[K", 3);
-          render_buffer_append(&E.render_buffer, "\033[u", 3);
-        }
-        if (E.file_cursor_col >= E.file.rows[E.file_cursor_row].length) {
-          E.file_cursor_col = MAX(E.file.rows[E.file_cursor_row].length - 1, 0);
-          render_set_cursor_position(
-              &E.render_buffer, E.file_cursor_row - E.render_row_offset + 1,
-              E.file_cursor_col + 1);
-        }
-      }
-      break;
-    case 'l':
-      if (E.file_cursor_col < E.file.rows[E.file_cursor_row].length - 1) {
-        E.file_cursor_col++;
-        render_buffer_append(&E.render_buffer, "\033[C", 4);
-      }
-      break;
-    case 'h':
-      if (E.file_cursor_col > 0) {
-        E.file_cursor_col--;
-        render_buffer_append(&E.render_buffer, "\033[D", 3);
-      }
-      break;
-    }
-
+  while (editor_process_input(&E)) {
     render_buffer_write(&E.render_buffer);
   }
 
